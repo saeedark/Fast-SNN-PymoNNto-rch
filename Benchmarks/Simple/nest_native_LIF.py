f98e4486f59f7c1b8b9308870f3e94fdae56ecc8
Saeed <saeed.ali.rahimi@gmail.com>
2023-11-28 18:58:04 +0330

    fix: PyNN threads

--- Benchmarks/Simple/nest_native_LIF.py
+++ Benchmarks/Simple/nest_native_LIF.py
@@ -8,7 +8,7 @@ from globparams import *
 from pynestml.codegeneration.nest_code_generator_utils import NESTCodeGeneratorUtils
 
 #############################################################
-#model generation
+# model generation
 #############################################################
 
 simple_neuron_str = """
@@ -18,7 +18,7 @@ neuron simple_neuron:
         v mV = 0 mV
 
     equations:
-        v' = (-v * decay) / ms
+        v' = (I_e/pA * mV - v * decay) / ms
 
     parameters:
         vt real = 6.1
@@ -29,19 +29,19 @@ neuron simple_neuron:
 
     input:
         spikes mV <- spike
+        I_e pA <- continuous
 
     output:
         spike
 
     update:
+        integrate_odes()
+        v += spikes*input_strength
+
         # threshold crossing
         if v >= vt * mV:
             v = vr * mV
             emit_spike()
-
-        integrate_odes()
-
-        v += random_uniform(0,1) * mV + spikes * input_strength
 """
 
 simple_stdp_synapse = """
@@ -75,22 +75,27 @@ synapse stdp_nn_symm:
         deliver_spike(w, d)
 """
 
-module_name, neuron_model_name, synapse_model_name = NESTCodeGeneratorUtils.generate_code_for(
-    simple_neuron_str,
-    simple_stdp_synapse,
-    post_ports=["post_spikes"])
+(
+    module_name,
+    neuron_model_name,
+    synapse_model_name,
+) = NESTCodeGeneratorUtils.generate_code_for(
+    simple_neuron_str, simple_stdp_synapse, post_ports=["post_spikes"]
+)
 
 nest.Install(module_name)
 
 
 #############################################################
-#simulation
+# simulation
 #############################################################
 
 
 # Set up the NEST simulation
 nest.ResetKernel()
-nest.SetKernelStatus({'resolution': 1.0, 'print_time': False, 'local_num_threads': os.cpu_count()})
+nest.SetKernelStatus(
+    {"resolution": 1.0, "print_time": False, "local_num_threads": os.cpu_count()}
+)
 
 # Define parameters
 num_neurons = SIZE
@@ -98,53 +103,53 @@ simulation_time = DURATION  # ms
 dt = 1.0  # ms
 
 # Create neurons
-neuron_params = {'vt': VT,
-                 'vr': VR,
-                 'decay': OM_DECAY}
+neuron_params = {"vt": VT, "vr": VR, "decay": OM_DECAY}
 neurons = nest.Create(neuron_model_name, num_neurons, params=neuron_params)
 
 # Create synapses
-synapse_params = {'synapse_model': synapse_model_name,
-                  'w': nest.random.uniform(min=0.0, max=1.0/num_neurons),
-                  'stdp_speed': STDP_SPEED}
-nest.Connect(neurons, neurons, 'all_to_all', synapse_params)
+synapse_params = {
+    "synapse_model": synapse_model_name,
+    "w": nest.random.uniform(min=0.0, max=1.0 / num_neurons),
+    "stdp_speed": STDP_SPEED,
+}
+nest.Connect(neurons, neurons, "all_to_all", synapse_params)
 
 
-#add voltage fluctuations to neurons
+# add voltage fluctuations to neurons
 
-# for i in range(num_neurons):
-#     times = list(np.arange(1.0, 101.0, 1.0))
-#     values = list(np.random.rand(int(simulation_time)))
-#     ng = nest.Create('step_current_generator')
-#     ng.set({"amplitude_times": times, "amplitude_values": values})
-#     nest.Connect(ng, neurons[i])
+for i in range(num_neurons):
+    times = list(np.arange(1.0, simulation_time * 1.0 + 1, 1.0 * dt))
+    values = list(np.random.rand(int(simulation_time)))
+    ng = nest.Create("step_current_generator")
+    ng.set({"amplitude_times": times, "amplitude_values": values})
+    nest.Connect(ng, neurons[i])
 
-#random noise
-#times = list(np.arange(1.0, 100.0, 1.0))
-#values = list(np.random.rand(99))
-#ng = nest.Create('step_current_generator', num_neurons)
-#ng.set({"amplitude_times": times, "amplitude_values": nest.random.uniform(min=0.0, max=1.0)})
-#nest.Connect(ng, neurons, "one_to_one")
+# random noise
+# times = list(np.arange(1.0, 100.0, 1.0))
+# values = list(np.random.rand(99))
+# ng = nest.Create('step_current_generator', num_neurons)
+# ng.set({"amplitude_times": times, "amplitude_values": nest.random.uniform(min=0.0, max=1.0)})
+# nest.Connect(ng, neurons, "one_to_one")
 
 if PLOT:
     sr = nest.Create("spike_recorder")
     nest.Connect(neurons, sr)
 
 
-#print(f"Start time: {time.time()}")
-#start = time.time()
-nest.Simulate(1/dt)
-#print(time.time()-start)
-#print(f"End time: {time.time()}")
+# print(f"Start time: {time.time()}")
+# start = time.time()
+nest.Simulate(1 / dt)
+# print(time.time()-start)
+# print(f"End time: {time.time()}")
 
 start = time.time()
-nest.Simulate(simulation_time - 1/dt)
+nest.Simulate(simulation_time - 1 / dt)
 print("simulation time: ", time.time() - start)
 
 if PLOT:
-    spike_rec=nest.GetStatus(sr, keys='events')[0]
+    spike_rec = nest.GetStatus(sr, keys="events")[0]
     print(f"Total spikes: {len(spike_rec['times'])}")
-    plt.plot(spike_rec['times'], spike_rec['senders'], '.k')
+    plt.plot(spike_rec["times"], spike_rec["senders"], ".k")
     plt.ylabel("neurons")
     plt.xlabel("t")
     plt.show()
