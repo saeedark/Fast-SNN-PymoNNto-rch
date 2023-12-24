2ae489bd9a4cea299b2d0c9733ab93c2d44d8b5a
Saeed <saeed.ali.rahimi@gmail.com>
2023-12-25 01:44:45 +0330

    feat: sparse sample

---
+++ Benchmarks/Simple/pymonnto_sparse.py
@@ -0,0 +1,94 @@
+from PymoNNto import *
+import time
+from globparams import *
+import scipy as sp
+
+DENSITY = 0.01
+settings = {'dtype': float32, 'synapse_mode': SxD}
+
+
+class LeakyIntegrateAndFire(Behavior):
+    def initialize(self, neurons):
+        neurons.spikes = neurons.vector('bool')
+        neurons.spikesOld = neurons.vector('bool')
+        neurons.voltage = neurons.vector()
+        self.threshold = self.parameter('threshold')
+        self.decay = self.parameter('decay')
+
+    def iteration(self, neurons):
+        neurons.spikesOld = neurons.spikes.copy()
+        neurons.spikes = neurons.voltage > self.threshold
+        #print(np.sum(neurons.spikes))# number of active neurons around 1.5%
+        #neurons.voltage.fill(0.0)
+        neurons.voltage *= np.invert(neurons.spikes) #reset VR
+        neurons.voltage *= self.decay #voltage decay
+
+
+
+class Input(Behavior):
+    def initialize(self, neurons):
+        for s in neurons.synapses(afferent, 'GLU'):
+            # s.W = s.matrix('random')
+            s.W = sp.sparse.random(*s.matrix_dim(), density=DENSITY)
+            s.col_idx = s.W.col
+            s.row_idx = s.W.row
+            s.W = s.W / SIZE
+            # s.W /= np.sum(s.W, axis=0) #normalize during initialization
+
+    def iteration(self, neurons):
+        neurons.voltage += neurons.vector('random')
+        for s in neurons.synapses(afferent, 'GLU'):
+            # s.dst.voltage += np.sum(s.W[s.src.spikes], axis=0)
+            s.dst.voltage += s.W.dot(s.src.spikes)
+
+
+class STDP(Behavior):
+    def initialize(self, neurons):
+        self.speed = self.parameter('speed')
+
+    def iteration(self, neurons):
+        for s in neurons.synapses(afferent, 'GLU'):
+            # mask = np.ix_(s.src.spikesOld, s.dst.spikes)
+            # s.W[mask] += self.speed
+            mask = s.dst.spikes[s.row_idx] * s.src.spikesOld[s.col_idx]
+            data = s.W.data
+            data[mask] += self.speed
+            data[mask] = np.clip(data[mask], 0.0, 1.0)
+            
+
+
+#class Norm(Behavior):
+#    def iteration(self, neurons):
+#        if neurons.iteration % 10 == 9:
+#            for s in neurons.synapses(afferent, 'GLU'):
+#                s.W /= np.sum(s.W, axis=0)
+
+
+net = Network(settings=settings)
+NeuronGroup(net, tag='NG', size=SIZE, behavior={
+    1: LeakyIntegrateAndFire(threshold=VT, decay=DECAY),
+    2: Input(),
+    3: STDP(speed=STDP_SPEED),
+    #4: Norm(),
+})
+
+if PLOT:
+    net.NG.add_behavior(9, EventRecorder('spikes'), False)
+
+SynapseGroup(net, src='NG', dst='NG', tag='GLU')
+net.initialize()
+
+if not MEASURE_INDIVIDUALLY:
+    start = time.time()
+    net.simulate_iterations(DURATION, batch_size=DURATION, measure_block_time=True)
+    print("simulation time: ", time.time() - start)
+else:
+    for i in range(DURATION):
+        net.simulate_iteration(measure_behavior_execution_time=True)
+    print('module time:', net.time_measures)
+
+if PLOT:
+    print('firing rate:', len(net['spikes.i', 0]) / SIZE / DURATION * 1000, 'Hz')
+    print(f"Total spikes: {len(net['spikes.i', 0])}")
+    plt.plot(net['spikes.t', 0], net['spikes.i', 0], '.k')
+    plt.show()
---
+++ Benchmarks/Simple/pymonntorch_sparse.py
@@ -0,0 +1,94 @@
+from pymonntorch import *
+import time
+import torch
+from matplotlib import pyplot as plt
+import numpy as np
+from globparams import *
+
+DENSITY = 0.01
+settings = {'dtype': torch.float32, 'synapse_mode': "SxD", 'device': 'cpu'}
+
+
+class LeakyIntegrateAndFire(Behavior):
+    def initialize(self, neurons):
+        neurons.spikes = neurons.vector(dtype=torch.bool)
+        neurons.spikesOld = neurons.vector(dtype=torch.bool)
+        neurons.voltage = neurons.vector()
+        self.threshold = self.parameter('threshold')
+        self.decay = self.parameter('decay')
+
+    def forward(self, neurons):
+        neurons.spikesOld = neurons.spikes.clone()
+        neurons.spikes = neurons.voltage > self.threshold
+        #print(np.sum(neurons.spikes))# number of active neurons around 1.5%
+        #neurons.voltage.fill(0.0)
+        neurons.voltage *= ~neurons.spikes #reset
+        neurons.voltage *= self.decay #voltage decay
+
+
+
+class Input(Behavior):
+    def initialize(self, neurons):
+        for s in neurons.synapses('afferent', 'GLU'):
+            # s.W = s.matrix('random')
+            n_row, n_col = s.matrix_dim()
+            nnz = int(n_row * n_col * DENSITY)
+            s.col_idx = torch.randint(0, n_col, (nnz,), device=s.device)
+            s.row_idx = torch.randint(0, n_row, (nnz,), device=s.device)
+            indices = torch.stack([s.row_idx, s.col_idx])
+            values =  s.tensor(mode='uniform', dim=(nnz,))
+            s.W = torch.sparse_coo_tensor(indices, values, s.matrix_dim())
+            s.W = s.W / SIZE
+            # s.W /= torch.sum(s.W, axis=0) #normalize during initialization
+
+    def forward(self, neurons):
+        neurons.voltage += neurons.vector('random')
+        for s in neurons.synapses('afferent', 'GLU'):
+            # s.dst.voltage += torch.sum(s.W[s.src.spikes], axis=0)
+            s.dst.voltage += torch.mm(s.W, s.src.spikes.view(-1,1)*1.0).view(-1,)
+            # s.dst.voltage += torch.mv(s.W, s.src.spikes*1.0)
+
+
+class STDP(Behavior):
+    def initialize(self, neurons):
+        self.speed = self.parameter('speed')
+
+    def forward(self, neurons):
+        for s in neurons.synapses('afferent', 'GLU'):
+            # mask = (torch.where(s.src.spikesOld)[0].view(-1, 1), torch.where(s.dst.spikes)[0].view(1, -1))
+            mask = s.dst.spikes[s.row_idx] * s.src.spikesOld[s.col_idx]
+            data = s.W._values()[:]
+            data[mask] += self.speed
+            data[mask] = torch.clip(data[mask], 0.0, 1.0)
+            
+
+
+#class Norm(Behavior):
+#    def iteration(self, neurons):
+#        if neurons.iteration % 10 == 9:
+#            for s in neurons.synapses(afferent, 'GLU'):
+#                s.W /= np.sum(s.W, axis=0)
+
+
+net = Network(**settings)
+NeuronGroup(net=net, tag='NG', size=SIZE, behavior={
+    1: LeakyIntegrateAndFire(threshold=VT, decay=DECAY),
+    2: Input(),
+    3: STDP(speed=STDP_SPEED),
+    #4: Norm(),
+    #5: EventRecorder(['spikes'])
+})
+
+if PLOT:
+    net.NG.add_behavior(9, EventRecorder('spikes'), False)
+
+SynapseGroup(net=net, src='NG', dst='NG', tag='GLU')
+net.initialize()
+
+start = time.time()
+net.simulate_iterations(DURATION)
+print("simulation time: ", time.time()-start)
+
+if PLOT:
+    plt.plot(net['spikes.t', 0].cpu(), net['spikes.i', 0].cpu(), '.k')
+    plt.show()
